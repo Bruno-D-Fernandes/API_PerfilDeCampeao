36cf4a03960865917d7732031de3b9025726432f
Bruno Fernandes <BrunoFernandes2315@Outlook.com>
2025-08-31 18:09:49 -0300

    merge com versão antiga

--- database/migrations/2025_08_31_203710_create_clubes_table.php
+++ database/migrations/2025_08_31_203710_create_clubes_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('clubes', function (Blueprint $table) {
             $table->id();
-            $table->string('nomeClube');
+            $table->string('nomeClube')->unique();
             $table->string('cidadeClube');
             $table->string('estadoClube');
             $table->date('anoCriacaoClube');
-            $table->string('cnpjClube');
+            $table->string('cnpjClube')->unique();
             $table->string('enderecoClube');
             $table->string('bioClube');
             $table->timestamps();
