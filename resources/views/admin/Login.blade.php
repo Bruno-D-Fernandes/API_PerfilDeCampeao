ae4ad9ede8d00c488b07e349a46ca608029038f1
maria <mariaalmeida131295s@gmail.com>
2025-10-21 17:40:48 -0300

    login-adm

--- resources/views/admin/Login.blade.php
+++ resources/views/admin/Login.blade.php
@@ -5,17 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login Admin</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="public\css\admin\loginAdm.css">
+        <link rel="stylesheet" href="{{asset('css/admin/loginAdm.css')}}">
     </head>
     <body>
             <form action="" id="formLogin">
                 @csrf
-
-                <input type="text" name="email" required>
-                <br>
-                <input type="password" name="password" required>
-                <br>
+                <div class="box">
+                <input type="text" name="email" required placeholder="email do usuario">
+                <input type="password" name="password" required placeholder="senha">
+                <input type="password" name="password" required placeholder="confirmar senha">
                 <button type="submit">Login</button>
+                </div>
             </form>
 
         <script src="{{ asset('js/admin/login.js')}}"></script>
