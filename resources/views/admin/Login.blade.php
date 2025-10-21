d75b6fa56df02fd3b48ad3fd0321b6f5637d7f25
maria <mariaalmeida131295s@gmail.com>
2025-10-21 17:25:41 -0300

    pasta css

--- resources/views/admin/Login.blade.php
+++ resources/views/admin/Login.blade.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login Admin</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="public\css\admin\loginAdm.css">
     </head>
     <body>
             <form action="" id="formLogin">
                 @csrf
-                
+
                 <input type="text" name="email" required>
                 <br>
                 <input type="password" name="password" required>
